831d950891d89e5b95d2cb8da4cefc372bf027d8
João Vitor <joaovitorcarvalho777999@gmail.com>
2026-06-03 09:26:53 -0300

    Configurando login

--- script.js
+++ script.js
@@ -51,9 +51,17 @@
 
   let currentUser = null;
   const users = {
-    'admin': { hash: '70ce5f4702ba0648c6913fe26ef35bfa3de9ff66632c02ca43ba43da393d39bc', role: 'admin', name: 'Administrador' },
-    'user': { hash: '5e8842c1651a186ee7a063d5de69a741f32076b738323419837a72b2996153a7', role: 'user', name: 'Usuário' }
-  };
+  'admin': {
+    hash: 'dc313636608d984f15a8785d03c10c5189e1e1fd957ee99b083da08fb7f1e7c3',
+    role: 'admin',
+    name: 'Administrador'
+  },
+  'user': {
+    hash: '5e8842c1651a186ee7a063d5de69a741f32076b738323419837a72b2996153a7',
+    role: 'user',
+    name: 'Usuário'
+  }
+};
 
   let dados = [];
   let listaLocais = [];
